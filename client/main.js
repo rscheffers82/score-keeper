4a18cc1f57ae4d733a0565b4bf1449ed34ff9b95
Roy Scheffers <r.scheffers@gmail.com>
2017-03-27 18:19:06 -0400

    add refresh once data changes

--- client/main.js
+++ client/main.js
@@ -5,24 +5,6 @@ import { Tracker } from 'meteor/tracker';
 
 import { Players } from './../imports/api/players';
 
-Tracker.autorun( () => {
-  console.log('Players in MiniMongo: ', Players.find().fetch());
-});
-
-const players = [{
-  _id: '1',
-  name: 'Bas',
-  score: 23
-},{
-  _id: '2',
-  name: 'Zorana',
-  score: 33
-},{
-  _id: '3',
-  name: 'Roy',
-  score: -12
-}];
-
 const renderPlayers = (playersList) => {
   return playersList.map( (player) => {
     return <p key={player._id}>{player.name} has {player.score} point(s)</p>
@@ -30,12 +12,22 @@ const renderPlayers = (playersList) => {
 };
 
 Meteor.startup( () => {
-  let title = 'Score Keep';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      {renderPlayers(players)}
-    </div>
-  );
-  ReactDOM.render(jsx, document.getElementById('app'));
+
+  Tracker.autorun( () => {
+    var players = Players.find().fetch();
+    renderPlayers(players);
+
+    let title = 'Score Keep';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        {renderPlayers(players)}
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+  Players.insert({
+    name: 'Roy',
+    score: 11
+  });
 });
--- server/main.js
+++ server/main.js
@@ -2,17 +2,10 @@ import { Meteor } from 'meteor/meteor'
 import { Players } from './../imports/api/players';
 
 Meteor.startup( ()=> {
-  Players.insert({
-    name: 'Zorana',
-    score: 46
-  });
-  // ,{
+  // Players.insert({
   //   name: 'Zorana',
-  //   score: 33
-  // },{
-  //   name: 'Roy',
-  //   score: -12
-  // }
+  //   score: 46
+  // });
   // find() only returns a pointer to the documents. In order to retrieve the docs we use .fetch()
   console.log(Players.find().fetch());
 
