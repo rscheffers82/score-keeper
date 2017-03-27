e19fc30762a2480e755c7d7ca67a9e4cf9a9da4d
Roy Scheffers <r.scheffers@gmail.com>
2017-03-27 19:24:45 -0400

    Add buttons to + & - points and one for removing users

--- client/main.js
+++ client/main.js
@@ -7,7 +7,18 @@ import { Players } from './../imports/api/players';
 
 const renderPlayers = (playersList) => {
   return playersList.map( (player) => {
-    return <p key={player._id}>{player.name} has {player.score} point(s)</p>
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s)
+        <button onClick={() => {
+          Players.update(player._id, { $inc: {score: 1} });
+        }}>+</button>
+        <button onClick={() => {
+          Players.update(player._id, { $inc: {score: -1} });
+        }}>-</button>
+        <button onClick={()=> Players.remove({player._id }); }>x</button>
+      </p>
+    );
   });
 };
 
--- server/main.js
+++ server/main.js
@@ -1,12 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 import { Players } from './../imports/api/players';
 
-Meteor.startup( ()=> {
-  // Players.insert({
-  //   name: 'Zorana',
-  //   score: 46
-  // });
-  // find() only returns a pointer to the documents. In order to retrieve the docs we use .fetch()
-  console.log(Players.find().fetch());
+Meteor.startup( () => {
 
 });
